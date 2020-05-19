73f4614a41c03b68c8a5f2b635e6913a381ca0ab
Renee Ernst <renee.ernst@genmills.com>
2020-05-19 15:35:17 -0500

    Pattern details functionality added

--- ravelry_playground/puller.py
+++ ravelry_playground/puller.py
@@ -6,11 +6,12 @@ import pandas as pd
 def ravelry_get_data(user, pwd, path, ids=None, data=None):
     """
     Get response with basic authentication
-    :param user:
-    :param pwd:
-    :param path:
-    :param data:
-    :return:
+    :param user: username for API authentication
+    :param pwd: password for API authentication
+    :param path: path to pull data from (e.g., patterns)
+    :param ids: ids used when searching for specific items
+    :param data: additional parameters used when pulling data
+    :return: request result when successful, error w/status code when not
     """
     if not ids:
         url = f'https://api.ravelry.com/{path}.json'
@@ -38,6 +39,14 @@ def basic_json_normalize(
         create_id: str,
         override_id: str
 ):
+    """
+    Convert JSON data nested 1 level deep to pd.DataFrame
+    :param data: data to use for df
+    :param record_path: path in data to where records wanted are
+    :param create_id: id to create in data
+    :param override_id: id to rename
+    :return: pd.DataFrame with needed data
+    """
     df = pd.json_normalize(
         data=data,
         record_path=record_path
