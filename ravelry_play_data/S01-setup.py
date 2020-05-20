e272fb879b760167021162128abd5a2c85943311
Renee Ernst <renee.ernst@genmills.com>
2020-05-20 14:04:38 -0500

    Adding info on where to save data after pulling it.

--- ravelry_play_data/S01-setup.py
+++ ravelry_play_data/S01-setup.py
@@ -2,8 +2,15 @@ import os
 
 import cauldron as cd
 
-# Config - Set your chosen auth type here
+# Config - Set your chosen auth type and save loc here
 auth = 'oauth'  # Set to basic or oauth
+save_loc = 'gcp'  # Set to gcp or local, or adjust code for another location
+
+# Note for save_loc, for saving data locally rather than in the cloud, I
+# created a folder at the top level of the repo called output_data and
+# added that to the .gitignore. If you configure the code to save data
+# somewhere else, make sure it does not end up in git as that would violate
+# the terms of service for Ravelry (do not publicly share the data).
 
 cd.display.header(f'Running with {auth} authentication')
 
@@ -37,4 +44,6 @@ elif auth == 'oauth':
 else:
     raise RuntimeError('Invalid auth type')
 
+# Save variables for use in other steps
+cd.shared.save_loc = save_loc
 cd.shared.auth_info = auth_info
--- ravelry_play_data/S02-attribute_data.py
+++ ravelry_play_data/S02-attribute_data.py
@@ -1,18 +1,21 @@
-# ravelry_play_data
-import os
+# Pull Color and Yarn attributes data
 import cauldron as cd
+
 import ravelry_playground
 
 auth_info = cd.shared.auth_info
+save_loc = cd.shared.save_loc
 
-# Color attribute data
+# Get all possible color attribute data
 df_color_attributes = ravelry_playground.get_color_attributes(auth_info)
 
 cd.display.header('Color Attributes Data: ', level=2)
 cd.display.table(df_color_attributes.head())
 
-# Yarn attribute data
+# Get all possible yarn attribute data
 df_yarn_attributes = ravelry_playground.get_yarn_attributes(auth_info)
 
 cd.display.header('Yarn Attributes Data: ', level=2)
 cd.display.table(df_yarn_attributes)
+
+
